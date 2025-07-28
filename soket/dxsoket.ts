4df629703b45caf15065eadc3fd82567176b094b
git <client@funxdata.com>
2025-07-28 18:57:20 +0800

    fix update

---
+++ soket/dxsoket.ts
@@ -0,0 +1,4 @@
+/** 减法函数 */
+export function del(a: number, b: number): number {
+    return a - b;
+}
---
+++ sse/dxsse.ts
@@ -0,0 +1,4 @@
+/** 加法函数 */
+export function add(a: number, b: number): number {
+    return a + b;
+}
