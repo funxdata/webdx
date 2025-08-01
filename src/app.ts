18b94b2e5595a7547581c62e5db7d8ad22473e0f
git <client@funxdata.com>
2025-08-01 11:13:22 +0800

    update test files

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("app info");
+
+import { Dxhttp } from "../http/exhttp.ts";
+const request = new Dxhttp({
+  baseURL: 'https://user.funxdata.com/v1',
+  timeout: 5000
+});
+// https://user.funxdata.com/v1/user/self
+
+// 获取个人信息
+const init = async ()=>{
+    const req_data =  await request.get(`/user/self`)
+    console.log(req_data);
+}
+init();
