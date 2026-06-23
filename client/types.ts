8ab085d1010275e3014a92b7950e426562ca0ba1
dowell2020 <dowell@funxdata.com>
2026-06-23 10:04:56 +0800

    fix types

--- client/types.ts
+++ client/types.ts
@@ -1,4 +1,4 @@
-export * from "./dxhttp/types.ts";
-export * from "./dxsoket/types.ts";
+export * from "./http/types.ts";
+export * from "./soket/types.ts";
 export * from "./sse/types.ts";
 export * from "./tools/types.ts";
